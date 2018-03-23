af74ecb45870ee4e4ad5a6e20e4eaa78944301f4
TheRiven <Novajames276@gmail.com>
2018-03-23 17:38:16 +0000

    Added argument handling

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,45 @@
 extern crate mazegenerator;
 
 fn main() {
-    let height = 4096;
-    let width = 4096;
+
+    let mut args : Vec<String> = vec![];
+
+    for (i, v) in std::env::args().enumerate() { 
+        if i != 0 {
+            args.push(v);
+        }
+    }
+
+    if args.len() < 2 {
+        println!("Not enough arguments, make sure to include a Width and Height, in that order.");
+        std::process::exit(1);
+    }
+
+    let height = match args[0].parse::<u64>() {
+        Ok(num) => num,
+        Err(err) => {
+            println!("{}", err);
+            println!("Make sure you have entered a number for height! defaulting to 128.");
+            128
+        } 
+    };
+
+    let width = match args[1].parse::<u64>() {
+        Ok(num) => num,
+        Err(err) => {
+            println!("{}", err);
+            println!("Make sure you have entered a number for width! defaulting to 128.");
+            128
+        } 
+    };
 
     let height = if height % 2 == 0 {
-        println!("+1 to height!");
         height + 1
     } else {
         height
     };
 
     let width = if width % 2 == 0 {
-        println!("+1 to width!");
         width + 1
     } else {
         width
