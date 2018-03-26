015cd02543964f46c819453abee10731417c0370
TheRiven <Novajames276@gmail.com>
2018-03-26 15:50:58 +0100

    Added timers to display time taken

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use std::collections::HashMap;
+use std::time::Instant;
 
 mod maze;
 mod mazebuilder;
@@ -24,8 +25,11 @@ pub fn create_and_save_maze(maze_height: u64, maze_width: u64) -> HashMap<(u64, 
         maze_width
     };
 
+    // Setup Timer
+    let timer = Instant::now(); 
+
     let maze = mazebuilder::generate_maze(height, width);
-    println!("Maze Generated");
+    println!("Maze Generated in {:?}", timer.elapsed());
 
     save_maze(height, width, &maze);
 
@@ -34,6 +38,7 @@ pub fn create_and_save_maze(maze_height: u64, maze_width: u64) -> HashMap<(u64, 
 
 fn save_maze(height: u64, width: u64, maze: &HashMap<(u64, u64), maze::Node>) {
     println!("Saving image with height {} and width {}", height, width);
+    let timer = Instant::now();
     imagecontrol::generate_image(height as u32, width as u32, maze);
-    println!("Image saved!");
+    println!("Image saved in {:?}", timer.elapsed());
 }
