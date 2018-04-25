6c1a33519b369a0f2162a8b696b67c317f6c5c72
TheRiven <Novajames276@gmail.com>
2018-04-25 16:06:29 +0100

    Kruskel Work in progresss - working out set merging

--- src/lib.rs
+++ src/lib.rs
@@ -28,7 +28,8 @@ pub fn create_and_save_maze(maze_height: u32, maze_width: u32) -> HashSet<(u32, 
     // Setup Timer
     let timer = Instant::now();
 
-    let maze = mazebuilder::generate_maze(mazebuilder::Generator::DFS { height, width });
+    let _maze = mazebuilder::generate_maze(mazebuilder::Generator::DFS { height, width });
+    let maze = mazebuilder::generate_maze(mazebuilder::Generator::Kruskal { height, width });
     println!("Maze Generated in {:?}", timer.elapsed());
 
     save_maze(height, width, &maze);
