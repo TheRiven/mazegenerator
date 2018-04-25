fd721db5789f7314d32302a3254d4d36dcfc845b
TheRiven <Novajames276@gmail.com>
2018-04-25 16:06:29 +0100

    Kruskel Work in progresss - working out set merging

--- src/mazebuilder/kruskal.rs
+++ src/mazebuilder/kruskal.rs
@@ -1,36 +1,154 @@
-use std::collections::{HashMap, HashSet};
 use rand::distributions::{IndependentSample, Range};
-use super::maze::Direction;
+use std::collections::{HashMap, HashSet, VecDeque};
+use std::rc::{Rc, Weak};
+//use super::maze::Direction;
+
+#[derive(Eq, Hash, PartialEq)]
+struct Cell {
+    pub x: u32,
+    pub y: u32,
+}
+
+struct Wall {
+    pub cell_a: Weak<Cell>,
+    pub cell_b: Weak<Cell>,
+    pub x: u32,
+    pub y: u32,
+}
 
 pub fn kruskal(height: u32, width: u32) -> HashSet<(u32, u32)> {
-    // Set of cells
-    let mut cells: HashMap<(u32, u32), u32> = HashMap::new();
+    // List of cells in the maze
+    let mut cells: HashSet<Rc<Cell>> = HashSet::new();
 
+    // Map of Cell sets
+    let mut cell_sets: HashMap<&Rc<Cell>, u32> = HashMap::new();
+
+    // Create the Cells
     let mut y = 1u32;
-    let mut id = 1;
     while y < height {
         let mut x = 1u32;
         while x < width {
-            cells.insert((x, y), id);
+            let new_cell = Rc::new(Cell { x, y });
+            cells.insert(new_cell);
             x += 2;
-            id += 1;
+        }
+        y += 1;
+    }
+
+    // Create the Cell Sets
+    let mut id = 1;
+    cells.iter().for_each(|cell| {
+        cell_sets.insert(&cell, id);
+        id += 1;
+    });
+
+    // List of walls
+    let walls = create_wall_list(&cells);
+
+    // So long as there are walls left to look at,
+    // select a random wall and get the cells it connects
+    // if the cells belong to diffrent sets, turn the wall into a cell
+    // and join the two sets together
+
+    while walls.len() > 0 {
+        let wall = pick_random_wall(&walls);
+
+        let cell_a = wall.cell_a.upgrade().expect("Found a wall without a Cell_a");
+        let cell_b = wall.cell_b.upgrade().expect("Found a wall without a Cell_b");
+
+        let cell_a_set = cell_sets[&cell_a];
+        let cell_b_set = cell_sets[&cell_b];
+
+        if cell_a_set != cell_b_set {
+            // Join the sets together here and then add the wall (now cell)
+            // to it.
+            
         }
 
-        y += 1;
+        
     }
 
     // Keep track of what has now been visited
     let visited = HashSet::new();
 
-    // For each wall in some random order
-        // Get cells that neighbour this wall
-        // If 4 neighbours are avalible, pick a north/south or east/west pair
-        // if only 3 are available then select the appropriate pair
-
-        // if the cells divided by this wall belong to distinct sets:
-        // remove the current wall
-        // Join the sets together
-
+    
 
     visited
+}
+
+fn create_wall_list(cells: &HashSet<Rc<Cell>>) -> Vec<Wall> {
+    // List of walls
+    let mut walls: Vec<Wall> = Vec::new();
+
+    // Keep track of what has been visited
+    let mut visited = HashSet::new();
+
+    // For each cell, Find its neighbours and create a wall between them
+    // then move to the neighbours and repeat untill no more walls can be made.
+    let mut cell_stack: VecDeque<&Rc<Cell>> = VecDeque::new();
+    let first_cell = cells.get(&Cell { x: 0, y: 0 }).unwrap();
+    cell_stack.push_back(first_cell);
+
+    while cell_stack.len() > 0 {
+        let current = cell_stack
+            .pop_front()
+            .expect("No Cell Found in cell_stack!");
+        let cell_neighbours = find_cell_neighbours(&current, &cells);
+        visited.insert(current);
+
+        cell_neighbours.into_iter().for_each(|cell| {
+            if !visited.contains(cell) {
+                let mut x = (cell.x - current.x) as i32;
+                if x > 0 { x = x - 1};
+                if x < 0 { x = x + 1};
+                let mut y = (cell.y - current.y) as i32;
+                if y > 0 { y = y - 1};
+                if y < 0 { y = y + 1};
+
+                let x = (current.x as i32 + x) as u32;
+                let y = (current.y as i32 + y) as u32;
+
+                let new_wall = Wall {cell_a: Rc::downgrade(&current), cell_b: Rc::downgrade(&cell), x, y};
+                walls.push(new_wall);
+                cell_stack.push_back(cell);
+            }            
+        });
+
+    }
+
+    walls
+}
+
+fn pick_random_wall<'a>(walls: &'a Vec<Wall>) -> &'a Wall {
+    let between = Range::new(0, walls.len());
+    let mut rng = super::rand::thread_rng();
+    let random = between.ind_sample(&mut rng) as usize;
+    let chosen = &walls[random];
+
+    chosen
+}
+
+fn find_cell_neighbours<'a>(cell: &Cell, cells: &'a HashSet<Rc<Cell>>) -> Vec<&'a Rc<Cell>> {
+    let x = cell.x;
+    let y = cell.y;
+
+    let mut neighbour_list: Vec<&Rc<Cell>> = Vec::new();
+
+    if let Some(north) = cells.get(&Cell { x: x, y: y - 1 }) {
+        neighbour_list.push(north);
+    }
+
+    if let Some(east) = cells.get(&Cell { x: x + 1, y: y }) {
+        neighbour_list.push(east);
+    }
+
+    if let Some(south) = cells.get(&Cell { x: x, y: y + 1 }) {
+        neighbour_list.push(south);
+    }
+
+    if let Some(west) = cells.get(&Cell { x: x - 1, y: y }) {
+        neighbour_list.push(west);
+    }
+
+    neighbour_list
 }
