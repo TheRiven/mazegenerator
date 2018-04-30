13c5ed3d04a820c689c0f2bbe22c172ef5894486
TheRiven <Novajames276@gmail.com>
2018-04-30 15:38:27 +0100

    Added in debug prints and fixed a u32 subtraction issue
    Kruskal works but slows due to large wall counts on anything bigger then 20 x 20

--- src/mazebuilder/kruskal.rs
+++ src/mazebuilder/kruskal.rs
@@ -1,7 +1,7 @@
 use rand::distributions::{IndependentSample, Range};
 use std::collections::{HashMap, HashSet, VecDeque};
 use std::rc::{Rc, Weak};
-//use super::maze::Direction;
+use std::time::Instant;
 
 #[derive(Eq, Hash, PartialEq)]
 struct Cell {
@@ -23,6 +23,8 @@ pub fn kruskal(height: u32, width: u32) -> HashSet<(u32, u32)> {
     // Map of Cell sets
     let mut cell_sets: HashMap<Rc<Cell>, u32> = HashMap::new();
 
+    let timer = Instant::now();
+
     // Create the Cells and sets
     let mut id = 1;
     let mut y = 1u32;
@@ -38,14 +40,19 @@ pub fn kruskal(height: u32, width: u32) -> HashSet<(u32, u32)> {
         y += 2;
     }
 
+    println!("Kruskal - Cell Generation Done in {:?}", timer.elapsed() );
+
     // List of walls
+    let timer = Instant::now();
     let mut walls = create_wall_list(&cells);
+
+    println!("Kruskal - Wall Generation Done in {:?}", timer.elapsed());
 
     // So long as there are walls left to look at,
     // select a random wall and get the cells it connects
     // if the cells belong to diffrent sets, turn the wall into a cell
     // and join the two sets together
-
+    let timer = Instant::now();
     while walls.len() > 0 {
         let wall = pick_random_wall(&mut walls);
 
@@ -72,14 +79,18 @@ pub fn kruskal(height: u32, width: u32) -> HashSet<(u32, u32)> {
         }
     }
 
-    // All sets should have been joined together at this point, so 
+    println!("Kruskal - Set Merging Done in {:?}", timer.elapsed());
+
+    // All sets should have been joined together at this point, so
     // we should now have a map of cells that can be saved and solved
     // for compatability we currently need to return as a hashset of tuples
-    let mut visited : HashSet<(u32, u32)> = HashSet::new();
+    let mut visited: HashSet<(u32, u32)> = HashSet::new();
 
     for cell in cells.drain() {
         visited.insert((cell.x, cell.y));
     }
+
+    println!("Path generation finished.");
 
     visited
 }
@@ -94,8 +105,12 @@ fn create_wall_list(cells: &HashSet<Rc<Cell>>) -> Vec<Wall> {
     // For each cell, Find its neighbours and create a wall between them
     // then move to the neighbours and repeat untill no more walls can be made.
     let mut cell_stack: VecDeque<&Rc<Cell>> = VecDeque::new();
-    let first_cell = cells.get(&Cell { x: 1, y: 1 }).expect("Unable to find cell 1, 1 in cells set");
+    let first_cell = cells
+        .get(&Cell { x: 1, y: 1 })
+        .expect("Unable to find cell 1, 1 in cells set");
     cell_stack.push_back(first_cell);
+
+    println!("Cells: {}", cells.len());
 
     while cell_stack.len() > 0 {
         let current = cell_stack
@@ -103,6 +118,7 @@ fn create_wall_list(cells: &HashSet<Rc<Cell>>) -> Vec<Wall> {
             .expect("No Cell Found in cell_stack!");
         let cell_neighbours = find_cell_neighbours(&current, &cells);
         visited.insert(current);
+        println!("Neighbours: {}", cell_neighbours.len());
 
         cell_neighbours.into_iter().for_each(|cell| {
             if !visited.contains(cell) {
@@ -136,6 +152,8 @@ fn create_wall_list(cells: &HashSet<Rc<Cell>>) -> Vec<Wall> {
         });
     }
 
+    println!("Visited: {}", visited.len());
+
     walls
 }
 
@@ -154,20 +172,24 @@ fn find_cell_neighbours<'a>(cell: &Cell, cells: &'a HashSet<Rc<Cell>>) -> Vec<&'
 
     let mut neighbour_list: Vec<&Rc<Cell>> = Vec::new();
 
-    if let Some(north) = cells.get(&Cell { x: x, y: y - 1 }) {
-        neighbour_list.push(north);
+    if y > 1 {
+        if let Some(north) = cells.get(&Cell { x: x, y: y - 2 }) {
+            neighbour_list.push(north);
+        }
     }
 
-    if let Some(east) = cells.get(&Cell { x: x + 1, y: y }) {
+    if let Some(east) = cells.get(&Cell { x: x + 2, y: y }) {
         neighbour_list.push(east);
     }
 
-    if let Some(south) = cells.get(&Cell { x: x, y: y + 1 }) {
+    if let Some(south) = cells.get(&Cell { x: x, y: y + 2 }) {
         neighbour_list.push(south);
     }
 
-    if let Some(west) = cells.get(&Cell { x: x - 1, y: y }) {
-        neighbour_list.push(west);
+    if x > 1 {
+        if let Some(west) = cells.get(&Cell { x: x - 2, y: y }) {
+            neighbour_list.push(west);
+        }
     }
 
     neighbour_list
