6def5bd6eaab8f549096960e2bdd1401ab9d33da
TheRiven <Novajames276@gmail.com>
2018-05-31 21:34:21 +0100

    Added in a bunch of tests and continued working on the algo

--- src/mazebuilder/dfs_imperfect.rs
+++ src/mazebuilder/dfs_imperfect.rs
@@ -6,7 +6,7 @@ enum Cell {
     Wall { position: Position },
 }
 
-#[derive(Copy, Clone, PartialEq, Eq, Hash)]
+#[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
 struct Position {
     x: u32,
     y: u32,
@@ -25,8 +25,6 @@ struct Point {
 }
 
 struct Maze {
-    height: u32,
-    width: u32,
     map: HashMap<Position, Cell>,
 }
 
@@ -49,7 +47,7 @@ impl Maze {
         };
         map.insert(start_pos, start_node);
 
-        Maze { height, width, map }
+        Maze { map }
     }
 }
 
@@ -70,7 +68,7 @@ pub fn generate(height: u32, width: u32) -> HashSet<(u32, u32)> {
     while stack.len() > 0 {
         // Try to find a position to move to
         if let Some(next) = get_next_position(&maze, point.position) {
-            // if a position has been found then move to that position and remove the 
+            // if a position has been found then move to that position and remove the
             // wall between there and the old position.
 
             // Set the new position as the current point and add it to the stack.
@@ -80,19 +78,29 @@ pub fn generate(height: u32, width: u32) -> HashSet<(u32, u32)> {
         } else {
             // if no position is found then backtrack and try again.
             let next = stack.pop().expect("Nothing Left in the stack!");
-            point = Point {position: next};  
+            point = Point { position: next };
             // add this position to the list of ends for creating more paths.
         }
-              
     }
 
     convert_nodes_to_visited(maze)
 }
 
-fn move_to_position(maze: Maze, point: Point, target: Position) -> (Maze, Point) {
-    let movement_x =  target.x - point.position.x;
-    let movement_y = target.y - point.position.y;
+fn move_to_position(mut maze: Maze, point: Point, target: Position) -> (Maze, Point) {
+    // work out the middle position to get the wall cell
+    let movement_x = target.x as i32 - point.position.x as i32;
+    let movement_y = target.y as i32 - point.position.y as i32;
+    let wall_cell_position = Position {
+        x: (point.position.x as i32 - (movement_x / 2)) as u32,
+        y: (point.position.y as i32 - (movement_y / 2)) as u32,
+    };
 
+    // Change both cells to be nodes
+    maze.map.insert(target, Cell::Node {position: target});
+    maze.map.insert(wall_cell_position, Cell::Node {position: target});
+
+    // Change position to be at the new node
+    let point = Point { position: target};
 
     (maze, point)
 }
@@ -125,47 +133,51 @@ fn get_next_position(maze: &Maze, position: Position) -> Option<Position> {
     let next = pick_random_position(next_positions);
 
     match next {
-        Cell::Node{position} => Some(*position),
-        Cell::Wall{position} => Some(*position),
+        Cell::Node { position } => Some(*position),
+        Cell::Wall { position } => Some(*position),
     }
 }
 
 fn get_cell_from_direction(maze: &Maze, position: Position, dir: Direction) -> Option<&Cell> {
-    let target_position = get_direction(dir, position);
-
-    if target_position.x > maze.width || target_position.y > maze.height {
-        return None;
-    }
-
-    if target_position.x < 1 || target_position.y < 1 {
-        return None;
-    }
-
-    if let Some(cell) = maze.map.get(&target_position) {
-        return Some(cell);
+    if let Some(target_position) = get_direction(dir, position) {
+        if let Some(cell) = maze.map.get(&target_position) {
+            return Some(cell);
+        }
     }
 
     None
 }
 
-fn get_direction(dir: Direction, position: Position) -> Position {
+fn get_direction(dir: Direction, position: Position) -> Option<Position> {
     match dir {
-        Direction::North => Position {
-            x: position.x,
-            y: position.y - 2,
-        },
-        Direction::East => Position {
+        Direction::North => {
+            if position.y as i32 - 2 < 1 {
+                return None;
+            }
+
+            Some(Position {
+                x: position.x,
+                y: position.y - 2,
+            })
+        }
+        Direction::East => Some(Position {
             x: position.x + 2,
             y: position.y,
-        },
-        Direction::South => Position {
+        }),
+        Direction::South => Some(Position {
             x: position.x,
             y: position.y + 2,
-        },
-        Direction::West => Position {
-            x: position.x - 2,
-            y: position.y,
-        },
+        }),
+        Direction::West => {
+            if position.x as i32 - 2 < 1 {
+                return None;
+            }
+
+            Some(Position {
+                x: position.x - 2,
+                y: position.y,
+            })
+        }
     }
 }
 
@@ -196,24 +208,89 @@ mod tests {
     use super::*;
 
     #[test]
-    fn create_maze() {
+    fn test_create_maze() {
         let maze = Maze::new(21, 21);
         assert_eq!(maze.map.len(), 441);
     }
 
     #[test]
-    fn get_pos1() {
+    fn test_get_next_position() {
         let maze = Maze::new(21, 21);
-        let position = Position {x: 1, y: 1};
-        let test = get_next_position(&maze, position).expect("Failed to unwrap get next position result!");
+        let position = Position { x: 1, y: 1 };
+        let test =
+            get_next_position(&maze, position).expect("Failed to unwrap get next position result!");
 
-        assert!(
-            match test {
-                Position {x: 4, y: 1} => true,
-                Position {x: 1, y: 4} => true,
-                _ => false,
-            }
-        )
+        assert_eq!(check_positions(test), true);
+    }
+
+    fn check_positions(test: Position) -> bool {
+        match test {
+            Position { x: 3, y: 1 } => true,
+            Position { x: 1, y: 3 } => true,
+            _ => false,
+        }
+    }
+
+    #[test]
+    fn test_move_to_position() {
+        let maze = Maze::new(21, 21);
+        let point = Point {position: Position {x: 1, y:1}};
+        let position = Position {x: 3, y: 3};
+
+        let (new_maze, new_point) = move_to_position(maze, point, position); 
+
+        assert_eq!(new_point.position, position  );
+
+        let test = new_maze.map.get(&position).expect("test_move_to_position -- unable to find new node!");
+        assert_eq!(match test {
+            Cell::Node {position: _} => true,
+            Cell::Wall {position: _} => false,
+        }, true);
+
+        let test2 =  new_maze.map.get(&Position {x: 2, y: 2}).expect("test_move_to_position -- unable to find new node!");
+        assert_eq!(match test2 {
+            Cell::Node {position: _} => true,
+            Cell::Wall {position: _} => false,
+        }, true);
+    }
+
+    #[test]
+    fn test_random_position() {
+        let cell_a = Cell::Node {
+            position: Position { x: 20, y: 20 },
+        };
+        let cell_b = Cell::Node {
+            position: Position { x: 20, y: 20 },
+        };
+        let cell_c = Cell::Node {
+            position: Position { x: 20, y: 20 },
+        };
+
+        let list = vec![&cell_a, &cell_b, &cell_c];
+
+        let _test = pick_random_position(list);
+    }
+
+    #[test]
+    fn test_get_direction() {
+        let position = Position { x: 1, y: 1 };
+        let north_test = get_direction(Direction::North, position);
+        let east_test = get_direction(Direction::East, position);
+        let south_test = get_direction(Direction::South, position);
+        let west_test = get_direction(Direction::West, position);
+
+        assert_eq!(north_test, None);
+        assert_eq!(east_test.unwrap(), Position { x: 3, y: 1 });
+        assert_eq!(south_test.unwrap(), Position { x: 1, y: 3 });
+        assert_eq!(west_test, None);
+    }
+
+    #[test]
+    fn test_convert_nodes() {
+        let maze = Maze::new(21, 21);
+        let test = convert_nodes_to_visited(maze);
+
+        assert_eq!(test.len(), 1);
     }
 
 }
